815e8dadda1d79214083d793fbc98ae5352fc3af
DobriJS <dobri.dobrevski@gmail.com>
2022-08-18 14:39:32 +0300

    Separate slides data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
 import React, { useState } from 'react';
+import { slides, initialSlides } from './slidesData';
 import './style.css';
-
-const slides = [
-  {
-    title: 'title 1',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae lectus et tortor dapibus volutpat sed sed dui. Aenean pharetra, lacus nec mollis vestibulum, est tortor lacinia dui, nec fringilla lorem massa sed diam. Aliquam id dui in ex vehicula tempor non a neque. Vivamus blandit odio ex, eget pulvinar est eleifend quis. In eros massa, varius vitae vestibulum eu, tristique eu nibh. Duis ut eros in nulla malesuada pulvinar.',
-  },
-  {
-    title: 'title 2',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae lectus et tortor dapibus volutpat sed sed dui. Aenean pharetra, lacus nec mollis vestibulum, est tortor lacinia dui, nec fringilla lorem massa sed diam. Aliquam id dui in ex vehicula tempor non a neque. Vivamus blandit odio ex, eget pulvinar est eleifend quis. In eros massa, varius vitae vestibulum eu, tristique eu nibh. Duis ut eros in nulla malesuada pulvinar.',
-  },
-  {
-    title: 'title 3',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae lectus et tortor dapibus volutpat sed sed dui. Aenean pharetra, lacus nec mollis vestibulum, est tortor lacinia dui, nec fringilla lorem massa sed diam. Aliquam id dui in ex vehicula tempor non a neque. Vivamus blandit odio ex, eget pulvinar est eleifend quis. In eros massa, varius vitae vestibulum eu, tristique eu nibh. Duis ut eros in nulla malesuada pulvinar.',
-  },
-];
-
-const initialSlides = 0;
 
 export default function App() {
   const [currentSlide, setCurrentSlide] = useState(initialSlides);
---
+++ src/slidesData.js
@@ -0,0 +1,16 @@
+export const slides = [
+  {
+    title: 'title 1',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae lectus et tortor dapibus volutpat sed sed dui. Aenean pharetra, lacus nec mollis vestibulum, est tortor lacinia dui, nec fringilla lorem massa sed diam. Aliquam id dui in ex vehicula tempor non a neque. Vivamus blandit odio ex, eget pulvinar est eleifend quis. In eros massa, varius vitae vestibulum eu, tristique eu nibh. Duis ut eros in nulla malesuada pulvinar.',
+  },
+  {
+    title: 'title 2',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae lectus et tortor dapibus volutpat sed sed dui. Aenean pharetra, lacus nec mollis vestibulum, est tortor lacinia dui, nec fringilla lorem massa sed diam. Aliquam id dui in ex vehicula tempor non a neque. Vivamus blandit odio ex, eget pulvinar est eleifend quis. In eros massa, varius vitae vestibulum eu, tristique eu nibh. Duis ut eros in nulla malesuada pulvinar.',
+  },
+  {
+    title: 'title 3',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae lectus et tortor dapibus volutpat sed sed dui. Aenean pharetra, lacus nec mollis vestibulum, est tortor lacinia dui, nec fringilla lorem massa sed diam. Aliquam id dui in ex vehicula tempor non a neque. Vivamus blandit odio ex, eget pulvinar est eleifend quis. In eros massa, varius vitae vestibulum eu, tristique eu nibh. Duis ut eros in nulla malesuada pulvinar.',
+  },
+];
+
+export const initialSlides = 0;
