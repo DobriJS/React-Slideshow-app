84310ab9ba5e889aab3eb965e6df9e356059a80b
DobriJS <dobri.dobrevski@gmail.com>
2022-08-18 14:27:45 +0300

    Make logic for first and last slides

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
+
+const slides = [
+  {
+    title: 'title 1',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae lectus et tortor dapibus volutpat sed sed dui. Aenean pharetra, lacus nec mollis vestibulum, est tortor lacinia dui, nec fringilla lorem massa sed diam. Aliquam id dui in ex vehicula tempor non a neque. Vivamus blandit odio ex, eget pulvinar est eleifend quis. In eros massa, varius vitae vestibulum eu, tristique eu nibh. Duis ut eros in nulla malesuada pulvinar.',
+  },
+  {
+    title: 'title 2',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae lectus et tortor dapibus volutpat sed sed dui. Aenean pharetra, lacus nec mollis vestibulum, est tortor lacinia dui, nec fringilla lorem massa sed diam. Aliquam id dui in ex vehicula tempor non a neque. Vivamus blandit odio ex, eget pulvinar est eleifend quis. In eros massa, varius vitae vestibulum eu, tristique eu nibh. Duis ut eros in nulla malesuada pulvinar.',
+  },
+  {
+    title: 'title 3',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae lectus et tortor dapibus volutpat sed sed dui. Aenean pharetra, lacus nec mollis vestibulum, est tortor lacinia dui, nec fringilla lorem massa sed diam. Aliquam id dui in ex vehicula tempor non a neque. Vivamus blandit odio ex, eget pulvinar est eleifend quis. In eros massa, varius vitae vestibulum eu, tristique eu nibh. Duis ut eros in nulla malesuada pulvinar.',
+  },
+];
+
+const initialSlides = 0;
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+  const [currentSlide, setCurrentSlide] = useState(initialSlides);
+  const [isFirstSlide, setIsFirstSlide] = useState(true);
+  const [isLastSlide, setIsLastSlide] = useState(false);
+
+  const updateSlide = (nextSlide) => {
+    if (nextSlide === 0) {
+      setIsFirstSlide(true);
+    } else {
+      setIsFirstSlide(false);
+    }
+    if (nextSlide === slides.length - 1) {
+      setIsLastSlide(true);
+    } else {
+      setIsLastSlide(false);
+    }
+  };
+
+  return <div className="app"></div>;
 }
